6b22509158fa04a72e35564eebff46bdaa48cb72
BusisiwePieterson <pietersonbusi@gmail.com>
2025-03-29 08:12:55 +0200

    new commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 window.addEventListener('scroll', () => {
   document.querySelector('nav').classList.toggle
   ('window-scroll', window.scrollY > 100)
-})
+});
 
 // show/hide faq answer
 
@@ -21,4 +21,25 @@ faqs.forEach(faq => {
       icon.className = "uil uil-plus"
     }
   })
-})
+});
+
+// show/hide nav menu
+
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+  menu.style.display = "flex";
+  closeBtn.style.display = "inline-block";
+  menuBtn.style.display = "none";
+});
+
+const closeNav = () => {
+  menu.style.display = "none";
+  closeBtn.style.display = "none";
+  menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav)
+
